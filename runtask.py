80ab572fb8c91103e3ee4386b87bd7cc4ab7c10e
Pravir Chandra <chandra@list.org>
2023-04-10 18:54:52 -0400

    adding basic demucs functionality

--- runtask.py
+++ runtask.py
@@ -1,3 +1,4 @@
+import os
 import time
 import json
 import subprocess
@@ -17,18 +18,58 @@ _task_bin[Tasks.MIDI] = '/usr/local/bin/basic-pitch'
 _task_bin[Tasks.COVR] = '/bin/echo'
 
 def _run_key_bpm_finder(filebase):
+    # Build the command line to run
     cmdline = []
     cmdline.append('/usr/local/bin/python3.9')
     cmdline.append(_task_bin[Tasks.KBPM])
     cmdline.append(filebase)
+    # Execute the command
     process = subprocess.Popen(cmdline, 
                                stdout=subprocess.PIPE,
                                universal_newlines=True)
     stdout, _ = process.communicate()
+    # The tool outputs JSON so return it as a dict
     return json.loads(stdout)
 
+def _get_model():
+    # This is the model name we'll run, must be one of:
+    #   htdemucs_6s htdemucs htdemucs_ft mdx mdx_extra hdemucs_mmi
+    return "htdemucs_6s"
+
+def _stems_for_model(model):
+    stems = [ "bass", "drums", "other", "vocals"]
+    if model == "htdemucs_6s":
+        stems.append("guitar")
+        stems.append("piano")
+    return stems
+
 def _run_demucs(filebase):
-    time.sleep(30)
+    model = _get_model()
+    stems = _stems_for_model(model)
+    # Build the command line to run
+    cmdline = []
+    cmdline.append(_task_bin[Tasks.STEM])
+    cmdline.extend([ "-d", "cpu",
+                     "-n", model,
+                     "-o", WORK_DIR,
+                     "--filename", "{track}-{stem}.{ext}"
+                   ])
+    cmdline.append(filebase)
+    # Execute the command if we don't already have output
+    outbase = f"{WORK_DIR}/{model}/{os.path.basename(filebase)}"
+    stdout=None
+    if not os.path.exists(f"{outbase}-{stems[0]}.wav"):
+        process = subprocess.Popen(cmdline,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT,
+                                   universal_newlines=True)
+        stdout, _ = process.communicate()
+    # Build the dict to return to caller
+    ret = { "model": model, "stdout": stdout }
+    ret[model] = {}
+    for stem in stems:
+        ret[model][stem] = f"{outbase}-{stem}.wav"
+    return ret
 
 def _run_phaselimiter(filebase):
     time.sleep(30)
