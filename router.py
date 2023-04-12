f1af6ef84a9539264b206f49bdbaa161960b1667
Pravir Chandra <chandra@list.org>
2023-04-11 23:36:56 -0400

    added basic persistence to task queue

--- router.py
+++ router.py
@@ -3,6 +3,9 @@ import os
 from datetime import datetime
 import time
 import zmq
+import shelve
+
+SHELVE_FILENAME = "saved-queue"
 
 # Frontend socket to collect messages from API
 context = zmq.Context()
@@ -25,24 +28,29 @@ def _log(str):
     sys.stdout.write(f"[{timestamp}] [Router-{pid}] {str}\n")
     sys.stdout.flush()
 
-# Push and pop tasks here
-queue = []
+with shelve.open(SHELVE_FILENAME) as store:
+    # Push and pop tasks here
+    queue = []
 
-# Switch messages forever
-_log("Starting up router with PID: %d" % pid)
-while True:
-    time.sleep(1)
-    _log("Router is polling for new messages, queue depth: %d" % len(queue))
-    socks = dict(poller.poll())
+    # Switch messages forever
+    _log("Starting up router with PID: %d" % pid)
+    while True:
+        # Extract the queue from the shelf
+        queue = store['queue']
+        time.sleep(1)
+        _log("Router is polling for new messages, queue depth: %d" % len(queue))
+        socks = dict(poller.poll())
 
-    if socks.get(frontend) == zmq.POLLIN:
-        message = frontend.recv_multipart()
-        task, file_id = message[0].split()
-        _log("Frontend got task: %s" % message[0])
-        queue.append(message[0])
+        if socks.get(frontend) == zmq.POLLIN:
+            message = frontend.recv_multipart()
+            task, file_id = message[0].split()
+            _log("Frontend got task: %s" % message[0])
+            queue.append(message[0])
 
-    if len(queue) > 0 and socks.get(backend) == zmq.POLLIN:
-        address, empty, ready = backend.recv_multipart()
-        job = queue.pop(0)
-        _log("Worker %s reports as ready, sending task: %s" % (address.hex(), job))
-        backend.send_multipart([address, b'', job])
+        if len(queue) > 0 and socks.get(backend) == zmq.POLLIN:
+            address, empty, ready = backend.recv_multipart()
+            job = queue.pop(0)
+            _log("Worker %s reports as ready, sending task: %s" % (address.hex(), job))
+            backend.send_multipart([address, b'', job])
+        # Put the queue back into the shelf for persistence
+        store['queue'] = queue
