2247d9da841bf71b7787660f169a83f2b9fd84ef
Pravir Chandra <chandra@list.org>
2023-04-29 10:04:57 -0400

    cleanup dead code

--- tasks/soundkit.py
+++ tasks/soundkit.py
@@ -19,12 +19,7 @@ def execute(file_id, force=False):
     # Get the external file
     local_file = filestore.download_file(api.get_soundkit_file_url(file_id), scratch)
 
-    #metadata = helpers.get_audio_info(local_file)
-    #if not metadata:
-    #    return { 'message': f'File format not recognized', 'failed': True }
-    #fmt = metadata['format'].get('format_name')
-    #if fmt != 'wav' and fmt != 'aiff':
-    #    return { 'message': f'Not accepting this file format: {fmt}', 'failed': True }
+    # XXX: Should do some kind of validation that it's a ZIP
 
     # Store the original
     ret['soundkit'] = filestore.store_file(file_id, local_file, f"{Tasks.OGSK.value}.zip", FILESTORE_SOUNDKITS)
