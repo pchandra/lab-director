ea12989cc2658ea83b6190289089936c3295a915
Pravir Chandra <chandra@list.org>
2023-08-09 23:28:22 -0400

    add run guard and save run params

--- tasks/coverart.py
+++ tasks/coverart.py
@@ -6,6 +6,11 @@ from config import CONFIG as conf
 STABLE_DIFFUSION_DIR = conf['STABLE_DIFFUSION_DIR']
 
 def ondemand(tg, params, force=False):
+    # Short-circuit if the filestore already has assets we would produce
+    tg.add_private([ f"{Tasks.COVR.value}.json" ])
+    if not force and tg.check_keys():
+        return True, helpers.msg('Already done')
+
     prompt = params['prompt']
     job_id = params['job_id']
     # Execute the command
@@ -38,4 +43,8 @@ def ondemand(tg, params, force=False):
     for i, file in enumerate([ f"{tg.scratch}/{x}" for x in outfiles ]):
         f = tg.put_file(file, f"{Tasks.COVR.value}/pic{i}.png")
         ret['files'].append(f)
+    tempfile = f"{tg.scratch}/{Tasks.COVR.value}.json"
+    with open(tempfile, 'w') as f:
+        f.write(json.dumps(params, indent=2))
+    ret['output'] = tg.put_file(tempfile, f"{Tasks.COVR.value}.json")
     return True, ret
