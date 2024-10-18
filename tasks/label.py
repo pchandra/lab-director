db5828b9a032b42e54146c2f361ff148bc8f82c7
Pravir Chandra <chandra@list.org>
2024-10-18 19:29:01 -0400

    adding screen for file formats

--- tasks/label.py
+++ tasks/label.py
@@ -5,7 +5,12 @@ from config import CONFIG as conf
 def ondemand(tg, params, force=False):
     if tg.status['type'] not in [ 'label' ]:
         return False, helpers.msg('ID is not label type')
+
+    fmt = params.get('format', ['wav'])
+
     for i in tg.iterate_files():
-        print(i)
+        print(i.key)
+        if i.key[-3:].lower() in fmt:
+            print(f"MATCH")
 
     return True, {}
